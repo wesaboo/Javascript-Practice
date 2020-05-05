8fada843f5ce51572bc9e3770064c42085ae0df9
Wesley <wesaboo@gmail.com>
2020-05-05 03:09:53 -0700

    add button animation

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,6 +6,9 @@ for (var i=0; i<numberOfDrumButtons; i++) {
         var buttonInnerHTML= this.innerHTML;
         
         makeSound(buttonInnerHTML)
+
+        buttonAnimation(buttonInnerHTML);
+
     });    
 
 }
@@ -13,6 +16,8 @@ for (var i=0; i<numberOfDrumButtons; i++) {
 document.addEventListener("keydown", function(event){
     
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -57,3 +62,13 @@ function makeSound(key){
             break;
     }
 };
+
+function buttonAnimation(currentKey){
+
+    var activeButton =  document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100)
+
+}

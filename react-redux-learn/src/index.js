c52b4b42e9fb0d09422b47514acc1179fb81eb9a
Wesley <wesaboo@gmail.com>
2020-12-26 18:19:17 -0800

    create boilerplate redux code

--- react-redux-learn/src/index.js
+++ react-redux-learn/src/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+
+// STORE - Globalized State, accessed by all components
+
+// ACTION - Name of an action, INCREMENT in this case, describes what you want to do
+// function that returns an object
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// REDUCER - Modifies state using the provided action
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+// Display in console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH - Starts the action which is sent to the reducer
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

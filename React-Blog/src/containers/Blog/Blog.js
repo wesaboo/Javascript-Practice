9e5f998ca785e11135937d9450bdf966c32b090b
Wesley <wesaboo@gmail.com>
2021-01-23 20:05:10 -0800

    react blog, manipulate response from axios, adds author to response, create click handler, adds click handler to posts

--- React-Blog/src/containers/Blog/Blog.js
+++ React-Blog/src/containers/Blog/Blog.js
@@ -5,31 +5,51 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import post from "../../components/Post/Post";
 
 class Blog extends Component {
   state = {
     posts: [],
+    selectedPostId: null,
   };
 
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
+      const posts = response.data.slice(0, 4);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "Wes",
+        };
+      });
       this.setState({
-        posts: response.data,
+        posts: updatedPosts,
       });
       //console.log(response);
     });
   }
 
+  postSelectHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  };
+
   render() {
     const posts = this.state.posts.map((post) => {
-      return <Post key={post.id} title={post.title} />;
+      return (
+        <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectHandler(post.id)}
+        />
+      );
     });
 
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
